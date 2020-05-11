5796f09a17330e4592e655ff2cada196494e8ca6
Gustavo Aguiar <gugahoa@gmail.com>
2020-05-10 22:30:46 -0300

    add current_user to lv socket

--- lib/monis_app_web/live/page_live.ex
+++ lib/monis_app_web/live/page_live.ex
@@ -1,8 +1,14 @@
 defmodule MonisAppWeb.PageLive do
   use MonisAppWeb, :live_view
 
-  def mount(_params, session, socket) do
-    IO.puts "MonisAppWeb.PageLive: mount/3 #{inspect({session, socket})}"
+  alias MonisApp.Accounts
+
+  def mount(_params, %{"user_token" => token} = session, socket) do
+    IO.puts("MonisAppWeb.PageLive: mount/3 #{inspect({session, socket})}")
+
+    socket =
+      assign_new(socket, :current_user, fn -> Accounts.get_user_by_session_token(token) end)
+
     {:ok, socket}
   end
 end
