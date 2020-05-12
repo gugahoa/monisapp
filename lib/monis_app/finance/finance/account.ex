f8814af2077b77fea002c22ef292791dfcc48776
Gustavo Aguiar <gugahoa@gmail.com>
2020-05-12 20:28:53 -0300

    Add name field to Finance.Account

--- lib/monis_app/finance/finance/account.ex
+++ lib/monis_app/finance/finance/account.ex
@@ -3,9 +3,11 @@ defmodule MonisApp.Finance.Account do
   import Ecto.Changeset
 
   schema "accounts" do
-    field :amount, :decimal
+    field :amount, :decimal, default: 0
     field :is_active, :boolean, default: true
-    field :type, :string
+    field :type, :string, null: false
+    field :name, :string, null: false
+
     belongs_to :user, MonisApp.Accounts.User
 
     timestamps()
@@ -14,7 +16,7 @@ defmodule MonisApp.Finance.Account do
   @doc false
   def changeset(account, attrs) do
     account
-    |> cast(attrs, [:amount, :is_active, :type])
-    |> validate_required([:amount, :is_active, :type])
+    |> cast(attrs, [:amount, :is_active, :type, :name])
+    |> validate_required([:amount, :is_active, :type, :name])
   end
 end
--- priv/repo/migrations/20200511005603_create_accounts.exs
+++ priv/repo/migrations/20200511005603_create_accounts.exs
@@ -3,9 +3,10 @@ defmodule MonisApp.Repo.Migrations.CreateAccounts do
 
   def change do
     create table(:accounts) do
-      add :amount, :decimal
+      add :amount, :decimal, default: 0
       add :is_active, :boolean, default: false, null: false
-      add :type, :string
+      add :type, :string, null: false
+      add :name, :string, null: false
       add :user_id, references(:users, on_delete: :nothing)
 
       timestamps()
