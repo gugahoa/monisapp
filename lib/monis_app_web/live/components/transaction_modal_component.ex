e1ee30aeb7c9e94e240096a43eb42699d0369f46
Gustavo Aguiar <gugahoa@gmail.com>
2020-05-14 23:08:48 -0300

    Use form_for, text_input and label for modal forms

--- lib/monis_app_web/live/components/transaction_modal_component.ex
+++ lib/monis_app_web/live/components/transaction_modal_component.ex
@@ -3,25 +3,55 @@ defmodule MonisAppWeb.TransactionModalComponent do
   use Phoenix.HTML
 
   alias MonisApp.Finance
+  alias MonisAppWeb.TransactionForm
 
   def update(%{current_user: user} = assigns, socket) do
+    changeset = TransactionForm.changeset(%TransactionForm{}, %{})
+
     socket =
       socket
       |> assign(assigns)
-      |> assign(accounts: Finance.list_accounts(user))
-      
+      |> assign(:accounts, Finance.list_accounts(user))
+      |> assign(:changeset, changeset)
+
     {:ok, socket}
   end
 
   def handle_event(
         "change-forms",
-        %{"_target" => ["account"], "account" => search_term},
+        %{
+          "_target" => ["transaction_form", "account"],
+          "transaction_form" => %{"account" => search_term}
+        },
         socket
       ) do
-    {:noreply, assign(socket, :accounts, Finance.search_accounts(search_term))}
+    socket =
+      socket
+      |> assign(:accounts, Finance.search_accounts(search_term))
+      |> assign(
+        :changeset,
+        TransactionForm.changeset(socket.assigns[:changeset], %{
+          account: search_term
+        })
+      )
+
+    {:noreply, socket}
   end
 
-  def handle_event("change-forms", _, socket) do
+  def handle_event(
+        "change-forms",
+        %{"_target" => ["transaction_form" | _], "transaction_form" => transaction},
+        socket
+      ) do
+    {:noreply,
+     assign(
+       socket,
+       :changeset,
+       TransactionForm.changeset(socket.assigns[:changeset], transaction)
+     )}
+  end
+
+  def handle_event("create-transaction", _, socket) do
     {:noreply, socket}
   end
 end
