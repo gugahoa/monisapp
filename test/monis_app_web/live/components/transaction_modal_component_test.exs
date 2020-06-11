2989a9c95abcd2ba0757077e5551e1ffcf833442
Gustavo Aguiar <gugahoa@gmail.com>
2020-06-10 21:04:35 -0300

    Improve how add transaction modal works

--- test/monis_app_web/live/components/transaction_modal_component_test.exs
+++ test/monis_app_web/live/components/transaction_modal_component_test.exs
@@ -8,16 +8,52 @@ defmodule MonisAppWeb.TransactionModalComponentTest do
   test "open-modal event opens add transaction modal component", %{conn: conn} do
     {:ok, view, html} = live(conn, Routes.live_path(conn, MonisAppWeb.PageLive))
 
-    assert html =~ "open_transaction_modal: false"
+    refute html =~ "Add transaction"
+
+    assert view
+           |> element("#open-transaction-modal")
+           |> render_click() =~ "Add transaction"
+  end
+
+  test "form correctly creates transaction with positive amount when toggle is checked", %{
+    conn: conn,
+    user: user
+  } do
+    account = insert(:account, user: user)
+    category = insert(:category, user: user)
+
+    {:ok, view, _html} = live(conn, Routes.live_path(conn, MonisAppWeb.PageLive))
 
     assert view
            |> element("#open-transaction-modal")
            |> render_click() =~ "Add transaction"
 
-    assert render(view) =~ "open_transaction_modal: true"
+    assert {:ok, view, html} =
+             view
+             |> form("form[phx-submit='create-transaction']", %{
+               transaction: %{
+                 account_id: account.id,
+                 category_id: category.id,
+                 payee: "Payee",
+                 note: "Example note",
+                 toggle: true,
+                 amount: 15
+               }
+             })
+             |> render_submit()
+             |> follow_redirect(conn, Routes.live_path(conn, MonisAppWeb.PageLive))
+
+    assert html =~ "successfully created"
+    refute html =~ "Add transaction"
+
+    assert [%{amount: amount}] = MonisApp.Finance.list_transactions()
+    assert Decimal.positive?(amount)
   end
 
-  test "form correctly creates transaction", %{conn: conn, user: user} do
+  test "form correctly creates transaction with negative amount when toggle is not checked", %{
+    conn: conn,
+    user: user
+  } do
     account = insert(:account, user: user)
     category = insert(:category, user: user)
 
@@ -42,9 +78,9 @@ defmodule MonisAppWeb.TransactionModalComponentTest do
              |> follow_redirect(conn, Routes.live_path(conn, MonisAppWeb.PageLive))
 
     assert html =~ "successfully created"
+    refute html =~ "Add transaction"
 
-    assert render(view) =~ "open_transaction_modal: false"
-
-    assert MonisApp.Finance.list_transactions() != []
+    assert [%{amount: amount}] = MonisApp.Finance.list_transactions()
+    assert Decimal.negative?(amount)
   end
 end
