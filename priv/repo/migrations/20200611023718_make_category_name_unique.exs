a973cf1fc1ed7328184e3d3803e74e04bc29bb50
Gustavo Aguiar <gugahoa@gmail.com>
2020-06-27 15:55:41 -0300

    Use tailwindui for sign up page

--- priv/repo/migrations/20200611023718_make_category_name_unique.exs
+++ priv/repo/migrations/20200611023718_make_category_name_unique.exs
@@ -2,6 +2,6 @@ defmodule MonisApp.Repo.Migrations.MakeCategoryNameUnique do
   use Ecto.Migration
 
   def change do
-    create unique_index(:categories, [:name])
+    create unique_index(:categories, [:user_id, :name])
   end
 end
